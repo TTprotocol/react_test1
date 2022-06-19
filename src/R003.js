d8b18e0cc3b208f958d8869a2a43df523724a8f7
TTprotocol <wnsdud7941@naver.com>
2022-06-19 15:39:23 +0900

    R004 test

---
+++ src/R003.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+// 003
+class R003_Import extends Component {
+  render() {
+    return (
+      <h2>[THIS IS IMPORTED COMPONENT test]</h2>
+    )
+  }
+}
+
+export default R003_Import;
