50c1ec5f1d40060322209fe72ddc1c5b0300328d
TTprotocol <wnsdud7941@naver.com>
2022-06-19 18:12:21 +0900

    R005

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
 import React from 'react';
-import ImportConponent from './R004'
+import ImportConponent from './R005'
 
-// 001 ~ 002
 function App() {
   return (
+    
     <div className="App">
       <h1>Start React 200</h1>
       <p>HTML 적용하기</p>
--- src/index.js
+++ src/index.js
@@ -6,12 +6,13 @@ import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+  // <React.StrictMode>
     <App />
-  </React.StrictMode>
+  // </React.StrictMode>
+  /* Strict Mode : 리액트에서 제공하는 검사 도구.
+  개발 모드일 때만 디버그를 하며 해당 태그로 감싸져 있다면 자손까지 검사한다.
+  console.log가 2번 실행되는 원인.
+  */
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
